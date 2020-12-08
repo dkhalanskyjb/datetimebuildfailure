cf4955fe87195702bd5201c087508dfc967600ae
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2020-12-08 15:18:31 +0300

    Fix serializer

--- shared/src/commonMain/kotlin/com/example/myapplication/shared/LocalDateTimeSerializer.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/shared/LocalDateTimeSerializer.kt
@@ -4,14 +4,18 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 @ExperimentalSerializationApi
-@Serializer(forClass = LocalDateTime::class)
-class LocalDateTimeSerializer(override val descriptor: SerialDescriptor) :
-    KSerializer<LocalDateTime> {
+class LocalDateTimeSerializer: KSerializer<LocalDateTime> {
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("Instant", PrimitiveKind.STRING)
+
     override fun serialize(encoder: Encoder, value: LocalDateTime) {
         encoder.encodeString(value.toString())
     }
